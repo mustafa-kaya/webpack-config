2528fd10b7eeb8a3f1f163c4fdc9b187e2c605e6
Mustafa KAYA <iletisim.mustafakaya@gmail.com>
2022-07-16 13:42:06 +0300

    Add babel to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const elvenShieldRecipe = {
   learherStrips: 2,
   ironIngot: 1,
-  refinedMoonStone: 4
+  refinedMoonStone: 4,
+};
+
+const elvenGauntLetsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonStone: 4,
 };
 
 console.log(elvenShieldRecipe);
+console.log(elvenGauntLetsRecipe);
